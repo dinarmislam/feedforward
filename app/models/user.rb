8b3131bbaf1d16415561fd6de58cb3a25f17ef5c
Md. Dinar <dinar.cse@gmail.com>
2016-09-26 15:30:20 +0600

    fix registration issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :links
   has_many :votes
 
-  validates :first_name, :last_name, presence: true
+
 
   def name; "#{first_name} #{last_name}"; end
   def short_name; "#{first_name} #{last_name.first}."; end
